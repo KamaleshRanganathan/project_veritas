743e4ec557363cfc82bd3df1f0da9ee4956ea66b
kamalesh r <kamalesh20200@gmail.com>
2025-08-30 13:23:03 +0530

    updated the routes

--- Veritas-cli/src/app/app.routes.ts
+++ Veritas-cli/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Welcome } from './pages/welcome/welcome';
+import { authGuard } from './guards/auth-guard';
+export const routes: Routes = [
+    // If the user is logged in, the guard allows access to /home.
+    // If not, the guard will redirect them to /login.
 
-export const routes: Routes = [];
+    {
+        path:'',
+        component:Welcome
+    },
+    {
+        path:'home',
+        component:Home,
+        // canActivate:authGuard
+    },
+    {
+        path:'login',
+        component:Login,
+    },
+];

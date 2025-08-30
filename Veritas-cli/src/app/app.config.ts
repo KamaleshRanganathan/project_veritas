26023d863e2e05721d7aab79db93f2c65e11d754
kamalesh r <kamalesh20200@gmail.com>
2025-08-30 19:09:06 +0530

    modified  the keys in app.config

--- Veritas-cli/src/app/app.config.ts
+++ Veritas-cli/src/app/app.config.ts
@@ -4,6 +4,7 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { FormsModule } from '@angular/forms';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,15 +14,7 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom([
       FormsModule
     ]),
-    provideFirebaseApp(() => initializeApp({
-      apiKey: "AIzaSyBpewgnGu2ptB8Dl7cbUS-4Lf_wDtJE7AU",
-      authDomain: "project-veritas-45a63.firebaseapp.com",
-      projectId: "project-veritas-45a63",
-      storageBucket: "project-veritas-45a63.firebasestorage.app",
-      messagingSenderId: "224717493539",
-      appId: "1:224717493539:web:48e3951f971dd4bdd3a896",
-      measurementId: "G-RPW7JFTN70"
-    })),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth())
   ]
 };

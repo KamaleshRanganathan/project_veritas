61b39f9fd115ee0b7d21d1a99744bfe8c2a5b32c
kamalesh r <kamalesh20200@gmail.com>
2025-08-30 18:23:13 +0530

    final update on firebase creds

--- Veritas-cli/src/app/app.config.ts
+++ Veritas-cli/src/app/app.config.ts
@@ -1,12 +1,23 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+      apiKey: "AIzaSyBpewgnGu2ptB8Dl7cbUS-4Lf_wDtJE7AU",
+      authDomain: "project-veritas-45a63.firebaseapp.com",
+      projectId: "project-veritas-45a63",
+      storageBucket: "project-veritas-45a63.firebasestorage.app",
+      messagingSenderId: "224717493539",
+      appId: "1:224717493539:web:48e3951f971dd4bdd3a896",
+      measurementId: "G-RPW7JFTN70"
+    })),
+    provideAuth(() => getAuth())
   ]
 };

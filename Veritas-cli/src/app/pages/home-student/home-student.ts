feb7c03bb28c7617ddd91f9eb58b761183bdfec6
kamaleshRanganathan <kamalesh20200@gmail.com>
2025-09-28 13:45:42 +0530

    dummy groups

--- Veritas-cli/src/app/pages/home-student/home-student.ts
+++ Veritas-cli/src/app/pages/home-student/home-student.ts
@@ -1,9 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services/auth';
+import { Groups } from '../../component/groups/groups';
+import { CommonModule } from '@angular/common'; // Import CommonModule for *ngFor
+
+
+export interface Group {
+  id: number;
+  name: string;
+  date: string;
+  status : string;
+}
 
 @Component({
   selector: 'app-home-student',
-  imports: [],
+  imports: [CommonModule, Groups], 
   templateUrl: './home-student.html',
   styleUrl: './home-student.css'
 })
@@ -14,4 +24,13 @@ export class HomeStudent {
     this.authService.logout();
   }
   
+  groups: Group[] = [
+    { id: 1, name: 'MATHS DA -1 ', date: '2025-09-30', status: 'Completed' },
+    { id: 2, name: 'NOSQL DA -2', date: '2025-09-30' , status: 'Pending' },
+    { id: 3, name: 'RTA HOMEWORK', date: '2025-09-30', status: 'In Progress' }
+  ];
+
+  trackByGroupId(index: number, groups: Group): number {
+    return groups.id;
+  }
 }

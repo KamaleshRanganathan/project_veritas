9ec3b2ac3c9f84aaa7d9896949f2df26c2af1d18
kamaleshRanganathan <kamalesh20200@gmail.com>
2025-10-01 12:35:43 +0530

    connection with mongodb

---
+++ Veritas-serv/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async() => {
+    try{
+        const conn = await mongoose.connect(process.env.key,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`mongoDb connected: ${conn.connection.host}`)
+    }catch(e){
+        console.error(e.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
